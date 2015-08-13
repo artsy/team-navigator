a826bcea61a87bd7638b70ebbe77d328df647f59
Ash Furrow <ash@ashfurrow.com>
2015-08-13 12:48:53 -0400

    Basic search-as-you-type.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,17 @@
-function searchTextUpdated() {
-  var searchField = document.getElementById('search');
+function searchTextUpdated(searchField) {
   var searchText = searchField.value;
 
-  var members = $('.member')
+
+  $('.member').each(function(index) {
+    // console.log($(this).attr('data-name'));
+    $(this).removeClass('hidden');
+  });
+
+  $('.member').filter(function(index) {
+    return $(this).attr('data-name').indexOf(searchText) == -1;
+  }).each(function(index) {
+    $(this).addClass('hidden');
+  });
+
+  console.log("");
 }
