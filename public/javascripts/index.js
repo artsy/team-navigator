305a90eb1fbd5bc98f2830401c03ec34d3bdb59e
Ash Furrow <ash@ashfurrow.com>
2015-08-13 15:46:36 -0400

    subteam hiding.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,20 +1,42 @@
 function searchTextUpdated(searchField) {
   var searchText = searchField.value;
 
-
-  $('.member, .team').each(function(index) {
+  // Show all teams, all subteams, all members.
+  $('.member, subteam-name, .team').each(function(index) {
     $(this).removeClass('hidden');
   });
 
+  if (searchText.lengh == 0) { return; }
+
+  // Find any members whose names don't match, and hide them. 
   $('.member').filter(function(index) {
     return $(this).attr('data-name').toLowerCase().indexOf(searchText.toLowerCase()) == -1;
   }).each(function(index) {
     $(this).addClass('hidden');
   });
 
+  // Find any teams that have no visible members, and hide them.
   $('.team').filter(function(index) {
     return $(this).children('.team-members').children(':not(.hidden)').length == 0
   }).each(function(index) {
     $(this).addClass('hidden');
+  })
+
+  // Hide all subteams
+  $('h2.subteam-name').each(function(index){
+    $(this).addClass('hidden');
   });
+
+  // Find subteams belonging to visible members
+  var subteams = $('.member:not(.hidden)').map(function(index){ 
+    return $(this).attr('data-subteam')
+  }).filter(function(index) {
+    return $(this).length > 0;
+  })
+  
+  // For ever subteam of a visible member, show it.
+  for (i = 0; i < subteams.length; i++) {
+    var header = $('h2:contains(' + subteams[i] + ')').first()
+    header.removeClass('hidden')
+  }
 }
