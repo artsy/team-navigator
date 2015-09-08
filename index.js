934a3748beca5fcfb9f7c34f29f294d2ad1dbc75
dzucconi <mail@damonzucconi.com>
2015-09-08 17:45:22 -0400

    Adds request logger

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var _ = require('lodash');
-var path = require('path')
+var path = require('path');
 var express = require('express');
 var getTeam = require('./lib/get_team');
 var getMember = require('./lib/get_member');
 var crop = require('./lib/crop');
-
 var teamify = require('./lib/teamify');
 
 var app = express();
 
 app.set('view engine', 'jade');
 app.use(express.static(path.resolve(__dirname, './public')));
+app.use(require('morgan')('dev'));
 
 app.get('/', function(req, res, next) {
   var mapObj = _.compose(_.object, _.map);
