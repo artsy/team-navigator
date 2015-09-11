f5279607e9f9eb458ef145e22389e5ee41e61f0c
dzucconi <mail@damonzucconi.com>
2015-09-11 12:52:12 -0400

    Implement a full text, re-rendering filter

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,47 +1,27 @@
 $(function() {
   'use strict';
 
-  $('.js-search').on('input', function(e) {
-    var searchText = $(this).val();
+  var threshold, $results, $members;
 
-    // Show all teams, all subteams, all members.
-    $('.member, subteam-name, .team').each(function(index) {
-      $(this).removeClass('hidden');
+  threshold = 30;
+  $results = $('.js-results');
+  $members = $('.js-member');
+
+  $('.js-search').on('input', function() {
+    var query, $matches;
+
+    query = $(this).val().toLowerCase();
+    $matches = $members.filter(function() {
+      if (this.textContent.toLowerCase().indexOf(query) >= 0) return $(this);
     });
 
-    if (!searchText.length) return;
+    if (threshold >= $matches.length) {
+      var $rendered;
 
-    // Find any members whose names don't match, and hide them.
-    $('.member').filter(function(index) {
-      return $(this).attr('data-name').toLowerCase().indexOf(searchText.toLowerCase()) == -1;
-    }).each(function(index) {
-      $(this).addClass('hidden');
-    });
-
-    // Find any teams that have no visible members, and hide them.
-    $('.team').filter(function(index) {
-      return $(this).children('.team-members').children(':not(.hidden)').length === 0;
-    }).each(function(index) {
-      $(this).addClass('hidden');
-    });
-
-    // Hide all subteams
-    $('h2.subteam-name').each(function(index) {
-      $(this).addClass('hidden');
-    });
-
-    // Find subteams belonging to visible members
-    var subteams = $('.member:not(.hidden)').map(function(index) {
-      return $(this).attr('data-subteam');
-    }).filter(function(index) {
-      return $(this).length > 0;
-    });
-
-    // For ever subteam of a visible member, show it.
-    for (i = 0; i < subteams.length; i++) {
-      var header = $('h2:contains(' + subteams[i] + ')').first();
-      header.removeClass('hidden');
+      $rendered = $('<div class="team-members"></div>').html($matches.clone());
+      $results.html($rendered).show();
+    } else {
+      $results.empty().hide();
     }
-
   });
 });
