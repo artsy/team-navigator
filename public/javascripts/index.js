a40ba12a0b4c98535e706787e24deb7107e66456
dzucconi <mail@damonzucconi.com>
2016-02-22 15:57:47 -0500

    Threshold should respond to the number of items on page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,14 @@
 $(function() {
   'use strict';
 
-  var threshold, $teams, $header, $results, $members;
-
-  threshold = 30;
+  var $teams, $header, $results, $members, threshold;
 
   $teams = $('.js-teams');
   $header = $('.js-header');
   $results = $('.js-results');
   $members = $('.js-member');
+
+  threshold = Math.floor($members.length / 5) || ($members.length - 1);
 
   $('.js-search')
     .on('focus', function() {
@@ -21,20 +21,18 @@ $(function() {
       var query, $matches;
 
       query = $(this).val().toLowerCase();
+
       $matches = $members.filter(function() {
         if (this.textContent.toLowerCase().indexOf(query) >= 0) return $(this);
       });
 
       if (threshold >= $matches.length) {
         var $rendered;
-
         $teams.hide();
-
         $rendered = $('<div class="members"></div>').html($matches.clone());
         $results.html($rendered).show();
       } else {
         $teams.show();
-
         $results.empty().hide();
       }
     });
