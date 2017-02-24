4bcd47dc9f0061fbe1bdb1fe3c52d84791ca63ab
Orta Therox <orta.therox@gmail.com>
2017-02-24 11:30:07 +0000

    Decouples the source from our edge cases around subteams, and adds the ability to highlight a few teams via env vars

--- app/views/sidebar/locations-teams.js
+++ app/views/sidebar/locations-teams.js
@@ -1,7 +1,7 @@
 import veact from 'veact'
 import { state, filterMembers } from '../../controllers'
 import { type, mediumMargin, purpleRegular, teamNameToID } from '../lib'
-import { assign, sortBy } from 'lodash'
+import { assign, sortBy, filter } from 'lodash'
 
 const view = veact()
 
@@ -21,24 +21,33 @@ view.styles({
   }
 })
 
-// These are not true "top level" teams, but are important enough to
-// warrent inclusion in the results
-const extraTeams = ['Design', 'Performance Marketing', 'Collector Experience', 'Partner Success', 'Analytics', 'Publishing']
+// Support per-project custom teams etc
+const { STANDOUT_SUBTEAMS, HIGHLIGHT_TEAM_NAME, HIGHLIGHT_TEAMS} = process.env
 
-view.render(() =>
-  div(
+const extraTeams = STANDOUT_SUBTEAMS.split(",")
+const highlightTeams = HIGHLIGHT_TEAMS.split(",")
+
+view.render(() => {
+  const team = filter(sortBy([...extraTeams, ...state.get('teams')]), team => !highlightTeams.includes(team))
+  return div(
     h2('.h2', 'Locations'),
-    ul('.ul', sortBy(state.get('cities')).map((city) =>
+    ul('.ul', sortBy(state.get('cities')).map(city =>
       li('.li', {
         onClick: () => filterMembers({ city }),
         style: { color: city === state.get('curFilter') ? purpleRegular : '' }
       }, city))),
     br('.br'),
+    h2('.h2', HIGHLIGHT_TEAM_NAME),
+    ul('.ul', sortBy(highlightTeams).map(team =>
+      a({ href: `/team/${teamNameToID(team)}` },
+        li('.li', team)
+      ))),
     h2('.h2', 'Teams'),
-    ul('.ul', sortBy([...extraTeams, ...state.get('teams')]).map((team) =>
+    ul('.ul', team.map(team =>
       a({ href: `/team/${teamNameToID(team)}` },
         li('.li', team)
       ))))
+  }
 )
 
 export default view()
