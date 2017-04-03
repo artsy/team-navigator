53e0b0441e6dbd7466e6aae4a354b7499b2fe711
Craig Spaeth <craigspaeth@gmail.com>
2017-04-03 17:52:47 -0400

    Fix state bug when using a single instance of the CSV parser.

--- test/models/sync.js
+++ test/models/sync.js
@@ -14,7 +14,12 @@ describe('Sync', () => {
     request.get = sinon.stub().returns(request)
     sync.__set__('request', request)
     // Stub mongojs
-    db = { members: { save: sinon.stub().returns(Promise.resolve()), remove: sinon.stub() } }
+    db = {
+      members: {
+        save: sinon.stub().returns(Promise.resolve()),
+        remove: sinon.stub()
+      }
+    }
     sync.__set__('db', db)
     // Helper to run resolve function
     resolve = () => sync.middleware[0](ctx, next)
@@ -23,8 +28,33 @@ describe('Sync', () => {
     sync.__set__('Slack', Slack)
   })
 
+  it('can sync twice', async () => {
+    const sync = async () => {
+      request.get.returns(Promise.resolve({
+        text: 'name,title,email,reports_to\nOrta,Badass,orta@,db'
+      }))
+      await resolve()
+      db.members.save.firstCall.args.should.eql([{
+        name: 'Orta',
+        title: 'Badass',
+        email: 'orta@',
+        reportsTo: 'db',
+        handle: 'orta',
+        teamID: undefined,
+        subteamID: undefined,
+        productTeamID: undefined,
+        teamRank: 0
+      }])
+    }
+    await sync()
+    await sync()
+    ctx.res.sync.should.equal('success')
+  })
+
   it('syncs a csv of team members into mongo documents', async () => {
-    request.get.returns(Promise.resolve({ text: 'name,title,email,reports_to\nOrta,Badass,orta@,db' }))
+    request.get.returns(Promise.resolve({
+      text: 'name,title,email,reports_to\nOrta,Badass,orta@,db'
+    }))
     await resolve()
     db.members.save.firstCall.args.should.eql([{
       name: 'Orta',
