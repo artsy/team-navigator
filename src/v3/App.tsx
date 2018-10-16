20ba7a270b51796fd1216f104e65bef5da16b07a
Orta Therox <orta.therox@gmail.com>
2018-10-16 15:26:31 -0400

    WIP on showing the number of buy now artworks:

--- src/v3/App.tsx
+++ src/v3/App.tsx
@@ -1,9 +1,22 @@
 import * as React from "react"
 
-class App extends React.Component {
+class ReloaderApp extends React.Component {
+
   public render() {
-    return <h1>Hi</h1>
+    return <App/ >
   }
 }
 
-export default App
+class App extends React.Component {
+  public render() {
+    return <html>
+    <body style={{ textAlign: "center" }}>
+    <h1 style={{ fontSize: 400, marginBottom: 0 }}>2801</h1>
+    <h2 style={{ fontSize: 200, marginBottom: 0 }}>Buy Now<br/>Artworks</h2>
+    </body>
+    </html>
+  }
+}
+
+
+export default ReloaderApp
